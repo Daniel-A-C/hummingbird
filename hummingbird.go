5143d6d3b939ec9fc2db8bed0fbfd1665e5e314b
Daniel Carroll <danalexanderc@gmail.com>
2024-05-07 17:28:36 -0400

    basic motion

--- hummingbird.go
+++ hummingbird.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"os"
 	"time"
-
+    "path/filepath"
 	"github.com/gdamore/tcell/v2"
 )
 
 func PrintCurrentDir(s tcell.Screen) {
+	s.Clear()
 
 	currentDir, err := os.Getwd()
 	if err != nil { fmt.Println("Error reading directory:", err); return}
@@ -16,32 +17,29 @@ func PrintCurrentDir(s tcell.Screen) {
 	files, err := os.ReadDir(currentDir)
 	if err != nil { fmt.Println("Error reading directory:", err); return}
 
-	EmitStr(s, 0, 1, tcell.StyleDefault, currentDir)
+	_, h := s.Size()
+	EmitStrMid(s, h-1, tcell.StyleDefault, currentDir)
 	skipOffset := -1
 	for i, file := range files {
 		if i % 5 == 0 { skipOffset += 1 }
-		EmitStrMid(s, i + skipOffset, tcell.StyleDefault, file.Name())
+		if file.IsDir() {
+			EmitStrMid(s, i + skipOffset, tcell.StyleDefault, file.Name() + "/")
+		} else {
+			EmitStrMid(s, i + skipOffset, tcell.StyleDefault, file.Name())
+		}
 	}
 	s.Show()
 }
 
 func RunHummingbird(s tcell.Screen) (string){ 
     mapping := map[byte]int{
-        'a': 0,
-        's': 1,
-        'd': 2,
-        'f': 3,
-        'g': 4,
-        'h': 5,
-        'j': 6,
-        'k': 7,
-        'l': 8,
-        ';': 9,
+        'a': 0, 's': 1, 'd': 2, 'f': 3, 'g': 4, 'h': 5, 'j': 6, 'k': 7, 'l': 8, ';': 9,
+        'z': 10, 'x': 11, 'c': 12, 'v': 13, 'b': 14, 'n': 15, 'm': 16, ',': 17, '.': 18, '/': 19,
     }
 
 	run := true
 
-	EmitStr(s, 0, 0, tcell.StyleDefault, "hummingbird")
+	//EmitStr(s, 0, 0, tcell.StyleDefault, "hummingbird")
 	PrintCurrentDir(s)
 
 	for run {
@@ -59,8 +57,21 @@ func RunHummingbird(s tcell.Screen) (string){
 					files, err := os.ReadDir(currentDir)
 					if err != nil { fmt.Println("Error reading directory:", err); return "x"}
 
-					EmitStr(s, 0, 4, tcell.StyleDefault, files[index].Name())
+					if(index < len(files) && files[index].IsDir()) {
+						err = os.Chdir(files[index].Name())
+						if err != nil { fmt.Println("Error changing directory:", err); return "x"}
+
+						PrintCurrentDir(s)
+					}
 					s.Show()
+				}
+				if string(ev.Rune()) == "e" {
+					currentDir, err := os.Getwd()
+					if err != nil { fmt.Println("Error reading directory:", err); return "x"}
+
+					err = os.Chdir(filepath.Dir(currentDir))
+					if err != nil { fmt.Println("Error reading directory:", err); return "x"}
+					PrintCurrentDir(s)
 				}
 			}
 		}
@@ -68,7 +79,10 @@ func RunHummingbird(s tcell.Screen) (string){
 		time.Sleep(1 * time.Millisecond)
 	}
 
-	return "/Users/danielcarroll/code/hummingbird/testDir"
+	currentDir, err := os.Getwd()
+	if err != nil { fmt.Println("Error reading directory:", err); return "x"}
+	return currentDir
+	//return "/Users/danielcarroll/code/hummingbird/testDir"
 	//return "x"
 }
 
