0099e54f7a20d0c368a5f175061e43e9c3def6b4
Daniel Carroll <danalexanderc@gmail.com>
2024-05-08 18:12:35 -0400

    movement and hints

--- directoryUtils.go
+++ directoryUtils.go
@@ -16,9 +16,12 @@ func PrintCurrentDir() {
 	files, err := os.ReadDir(currentDir)
 	if err != nil { fmt.Println("Error reading directory:", err); return}
 
-	_, h := s.Size()
+	if !displayHiddenFiles { files = filterHiddenFiles(files) }
+
+	w, h := s.Size()
 	EmitStrMid(h-1, tcell.StyleDefault, currentDir)
 	skipOffset := -1
+	maxFilenameLen := 0
 	for i, file := range files {
 		if i % 5 == 0 { skipOffset += 1 }
 		if file.IsDir() {
@@ -26,8 +29,34 @@ func PrintCurrentDir() {
 		} else {
 			EmitStrMid(i + skipOffset, tcell.StyleDefault, file.Name())
 		}
+
+		if len(file.Name()) > maxFilenameLen { maxFilenameLen = len(file.Name()) }
+		if i >= h-5 { break }
 	}
+
+	skipOffset = -1
+	if displayHints {
+		keys := "asdfghjkl;zxcvbnm,./"
+		for i := 0; i < len(files); i++ {
+			if i % 5 == 0 { skipOffset += 1 }
+			EmitStr(w/2 - maxFilenameLen/2 - 3, i + skipOffset, tcell.StyleDefault, string(keys[i]) + ")")
+			if i >= h-5 || i >= 19 { break }
+		}
+	}
+
 	s.Show()
+}
+
+func filterHiddenFiles(files []os.DirEntry) []os.DirEntry {
+    filteredFiles := []os.DirEntry{}
+
+    for _, file := range files {
+        if file.Name()[0] != '.' {
+            filteredFiles = append(filteredFiles, file)
+        }
+    }
+
+    return filteredFiles
 }
 
 func GoUpDir() {
@@ -47,11 +76,12 @@ func ChangeDir(index int) {
 	files, err := os.ReadDir(currentDir)
 	if err != nil { fmt.Println("Error reading directory:", err); return}
 
+	if !displayHiddenFiles { files = filterHiddenFiles(files) }
+
 	if(index < len(files) && files[index].IsDir()) {
 		err = os.Chdir(files[index].Name())
 		if err != nil { fmt.Println("Error changing directory:", err); return}
 
 		PrintCurrentDir()
 	}
-
 }
--- hummingbird.go
+++ hummingbird.go
@@ -10,12 +10,15 @@ var s tcell.Screen
 
 // Eventually should belong in some sort of "settings" feature.
 var displayHiddenFiles = false
+var displayHints = true
+
 
 func RunHummingbird() (string){ 
-    inputMap := map[byte]int{
-        'a': 0, 's': 1, 'd': 2, 'f': 3, 'g': 4, 'h': 5, 'j': 6, 'k': 7, 'l': 8, ';': 9,
-        'z': 10, 'x': 11, 'c': 12, 'v': 13, 'b': 14, 'n': 15, 'm': 16, ',': 17, '.': 18, '/': 19,
-    }
+
+	var inputMap = map[byte]int{
+		'a': 0, 's': 1, 'd': 2, 'f': 3, 'g': 4, 'h': 5, 'j': 6, 'k': 7, 'l': 8, ';': 9,
+		'z': 10, 'x': 11, 'c': 12, 'v': 13, 'b': 14, 'n': 15, 'm': 16, ',': 17, '.': 18, '/': 19,
+	}
 
 	run := true
 	PrintCurrentDir()
@@ -32,7 +35,11 @@ func RunHummingbird() (string){
 				} else if ev.Key() == tcell.KeyEscape || string(ev.Rune()) == "q" || ev.Key() == tcell.KeyCtrlC {
 					run = false
 				} else if string(ev.Rune()) == "y" {
-
+					displayHiddenFiles = !displayHiddenFiles
+					PrintCurrentDir()
+				} else if string(ev.Rune()) == "u" { 
+					displayHints = !displayHints
+					PrintCurrentDir()
 				}
 			}
 		}
